481243902fba84d2a79b9f686c1c02667bbb388e
Anastasia Bogdanova <livenready@yandex.ru>
2019-11-23 13:10:52 +0300

    task-2, add repositories

---
+++ src/main/java/org/itstep/msk/app/repository/ApplicationRepository.java
@@ -0,0 +1,9 @@
+package org.itstep.msk.app.repository;
+
+import org.itstep.msk.app.entity.Application;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApplicationRepository extends JpaRepository<Application, Integer> {
+}
---
+++ src/main/java/org/itstep/msk/app/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package org.itstep.msk.app.repository;
+
+import org.itstep.msk.app.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
